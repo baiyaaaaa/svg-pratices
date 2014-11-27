42d1ade507fbc09a6a4e252dde42d0c1a42bce34
曾海平 <bigpet1991@gmail.com>
2014-11-27 17:58:29 +0800

    add svg-sprites

---
+++ inline-svg-sprites/gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+
+var svgSymbols = require('gulp-svg-symbols');
+var connect = require('gulp-connect');
+
+gulp.task('webserver', function(){
+  connect.server();
+})
+
+gulp.task('sprite', function(){
+  return gulp.src('assets/svg/*.svg')
+  .pipe(svgSymbols())
+  .pipe(gulp.dest('assets'));
+});
+
+gulp.task('default', function(){
+    gulp.run('sprite', 'webserver');
+});
